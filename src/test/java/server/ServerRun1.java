994f2a1cf4f3833132f21bf9226a6d16b9983b14
zhangyulong <zhangyulong04@meituan.com>
2018-11-29 18:12:07 +0800

    提交一些测试代码

--- src/test/java/server/ServerRun1.java
+++ src/test/java/server/ServerRun1.java
@@ -2,8 +2,6 @@ package server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
